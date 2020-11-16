7a68428c3f657df03674a1442778e53c348fde7a
Sree Lasya Vallabhaneni <sreelasyav@bigleaf.net>
2020-11-15 20:20:07 -0800

    sol:remove duplicate from the array solution 2

---
+++ remove_duplicates_sol2.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = list(set(nums))
+        print(nums.sort())
+        return len(nums)
